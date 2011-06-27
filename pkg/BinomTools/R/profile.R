3d92a8198f0c42e88fd03dafc15dbf6102232a3d
mkh <mkh@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-27 08:49:55 +0000

    Handle NA estimates and other changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/binomtools@16 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/BinomTools/R/profile.R
+++ pkg/BinomTools/R/profile.R
@@ -1,5 +1,6 @@
+## OBS - change function name to profile
 
-profBin <- function(object, which.par, alpha = 0.001, max.steps = 50,
+profBin <- function(object, which.par, alpha = 0.005, max.steps = 50,
                nsteps = 8, step.warn = 5, trace = F, ...)
 {
   ## Match and test input arguments
@@ -9,11 +10,9 @@ profBin <- function(object, which.par, alpha = 0.001, max.steps = 50,
     stop("Object must be of class glm")
   if(family(object)$family != 'binomial')
     stop("GLM object must be fitted with the binomial family")
-### Må der fittes med quasi-binomial?
   stopifnot(is.numeric(alpha) && length(alpha) == 1 &&
             alpha > 0 && alpha < 1)
   stopifnot(round(max.steps) > round(nsteps))
-  stopifnot(round(nsteps) > round(step.warn))
   stopifnot(round(nsteps) > 0 && round(step.warn) >= 0)
   max.steps <- round(max.steps)
   nsteps <- round(nsteps)
@@ -30,21 +29,25 @@ profBin <- function(object, which.par, alpha = 0.001, max.steps = 50,
   else w <- rep(1, n)
   if(!is.null(model.offset(mf))) o <- model.offset(mf)
   else o <- rep(0, n)
-### Tjekke om ovenstående går godt uanset dim(y), w og o
-  fam <- family(object)
   p <- length(pnames <- names(b0 <- coef(object)))
+  na.par <- is.na(b0)
   if(missing(which.par)) which.par <- 1:p
-  if(!is.vector(which.par, mode='integer') && !is.vector(which.par,
-               mode='character')) 
+  if(!is.numeric(which.par) && !is.character(which.par))
     stop("'which.par' must be a character or integer vector")
+  if(is.numeric(which.par)) {
+    if(any(abs(which.par - round(which.par)) > 0.0001)) {
+    which.par <- round(which.par)
+    warning("Non-integer element(s) in 'which.par' rounded off to the nearest integer") 
+    }
+  }
   if(is.character(which.par)) which.par <- match(which.par, pnames, 0)
   if(any(is.na(which.par)))
     stop("Invalid parameter argument(s) in 'which.par'")
   stopifnot(length(which.par) > 0)
-  std.err <- coef(summary(object))[,'Std. Error']
+  std.err <- sqrt(diag(vcov(object)))
   orig.dev <- deviance(object)
-  disp <- summary(object)$dispersion
-### skal kun bruges hvis quasi er tilladt 
+  lLik <- logLik(object)
+  fam <- family(object)
   ## Profile limit
   lroot.max <- qnorm(1 - alpha/2)
   ## Step length
@@ -56,8 +59,12 @@ profBin <- function(object, which.par, alpha = 0.001, max.steps = 50,
   ## with 'wp' fixed at ascending/descending values (using offset) and
   ## store signed likelihood root values and parameter values
   for(wp in which.par) {
-    X.wp <- X[, -wp, drop = FALSE] ## Design matrix without wp
-    par.wp <- b.wp <- b0[wp] ## Reset wp coefficient to initial value
+    if(na.par[wp]) next ## If coef is NA skip this and go to next wp
+    skip.wp <- na.par
+    skip.wp[wp] <- TRUE ## Exclude coef with NA and current wp from
+                        ## design matrix
+    X.wp <- X[, !skip.wp, drop = FALSE] ## Design matrix without wp
+    par.wp <- b0[wp] ## Reset wp coefficient to initial value
     lroot.wp <- 0 ## lroot at MLE
     wp.name <- pnames[wp] ## Name of wp parameter
     for(direction in c(-1, 1)) { ## Move down and then up
@@ -68,26 +75,22 @@ profBin <- function(object, which.par, alpha = 0.001, max.steps = 50,
       }
       step <- 0
       lroot <- 0
-      start <- b0[-wp]
-      ## Increment wp (in offset) and refit model until threshold is reached
+      start <- b0[!skip.wp]
+      ## Increment wp (using offset) and refit model until threshold is
+      ## reached 
       while(step < max.steps && abs(lroot) <= lroot.max) {
         step <- step + 1
-        bi <- b.wp + direction * step * delta * std.err[wp]
-        oi <- o + X[, wp] * bi
-        attributes(oi) <- NULL
+        bi <- b0[wp] + direction * step * delta * std.err[wp.name]
+        oi <- as.vector(o + X[, wp] * bi)
         fm <- glm.fit(x = X.wp, y = y, weights = w, start = start,
                       offset = oi, family = fam, control = object$control)
-### Tage højde for om intercept er med??
-        ## Likelihood ratio statistic
-        lrs <- (fm$deviance - orig.dev)/disp 
-### Likelihood ratio statistic # er det rigtigt og gælder det
-### generelt? - skal dispersion være med?
+        dev <- (fm$deviance - orig.dev)
         ## Likelihood root statistic
-        lroot <- direction * sqrt(lrs) 
+        lroot <- direction * sqrt(dev) 
         lroot.wp <- c(lroot.wp, lroot)
         par.wp <- c(par.wp, bi)    
         ## Update start values
-        start <- coef(fm) 
+        start <- coef(fm)
       } ## end 'while step <= max.steps and lroot <= lroot.max'
       ## test that lroot.max is reached (e.g. max.steps not reached)
       if(abs(lroot) < lroot.max)
@@ -104,110 +107,139 @@ profBin <- function(object, which.par, alpha = 0.001, max.steps = 50,
     prof.list[[wp.name]] <-
       structure(data.frame(par.wp[par.order]), names = "par.values")
     prof.list[[wp.name]]$lroot <- lroot.wp[par.order]
-
-### Fikse warning    
-###    if(!all(diff(lroot.wp[par.order, wp.name]) > 0))
-###      warning("likelihood is not monotonically decreasing from maximum,\n",
-###              "  so profile may be unreliable for ", wb.name)
-    
+    if(!all(diff(lroot.wp[par.order]) > 0))
+      warning("likelihood is not monotonically decreasing from maximum,\n",
+              "  so profile may be unreliable for ", wp.name)
   } ## end 'for wp in which.par'
-  val <- structure(prof.list, summary.originalfit = summary(object),
-  name.originalfit = name.object, call.originalfit = call.object) 
+  val <- structure(prof.list, original.fit = object, summary =
+    summary(object), name.originalfit = name.object, call.originalfit =
+    call.object)  
   class(val) <- c('profBin.glm')
   return(val)
 }
-pr1 <- profBin(Serum.glm)
-pr1
 
 
 
 #######################
 ## Plot of profile likelihood
 #######################
-# plot.profBin.glm - senere navn?
+
+## OBS - change function name to plot.profile
 aa <- function(x, which.par, statistic = c("likelihood", "lroot"),
-                             log = TRUE, approx = TRUE,
-                             conf.int = TRUE, level = 0.95,
-                             fig = TRUE)
-                             # flere senere: n = ???, ylim = ...?, ...) 
+                             relative = TRUE, log = FALSE, approx = TRUE,
+                             conf.int = TRUE, level = 0.95, n = 100, 
+                             fig = TRUE, ylim = NULL, ...)
 {  
   ## Match and test arguments
   xnames <- names(x)
   if(missing(which.par)) which.par <- seq_along(xnames)
-#  if(...) warning('should be character or numeric vector')
+  if(!is.numeric(which.par) && !is.character(which.par))
+    stop("'which.par' must be a character or integer vector")
+  if(is.numeric(which.par)) {
+    if(any(abs(which.par - round(which.par)) > 0.0001)) {
+    which.par <- round(which.par)
+    warning("Non-integer element(s) in 'which.par' rounded off to the nearest integer")
+    }
+    if(any(which.par > length(x)))
+       stop("One of more elements in 'which.par' exceedes number of parameters in x")
+  }
   if(is.character(which.par)) which.par <- match(which.par, xnames, 0)
-#  if(!all...match...) warning('nogle matcher ikke')
-#  if(which.par == 0) stop('ingen parametre matcher')
-  p <- length(which.par)
+  if(any(is.na(which.par)))
+    stop("Invalid parameter argument(s) in 'which.par'")
+  stopifnot(length(which.par) > 0)
   statistic <- match.arg(statistic)
-  ## Misch extraction from original glm object
-  orig.fit <- attr(x, 'summary.originalfit')
+  ## Match extraction from original glm object
+  orig.fit <- attr(x, 'original.fit')
   name.originalfit <- attr(x, 'name.originalfit')
   call.originalfit <- attr(x, 'call.originalfit') 
-  b0 <- coef(orig.fit)[, 'Estimate']
-  std.err <- coef(orig.fit)[, 'Std. Error'] # brug vcov i stedet for
+  b0 <- coef(orig.fit)
+  std.err <- sqrt(diag(vcov(orig.fit)))
+  lLik <- logLik(orig.fit)
+  par.null <- sapply(x, is.null)
+  if(any(par.null[which.par]))
+    warning(paste("Profile likelihood not shown for parameter '",
+                  names(which(par.null)),
+                  "' due to missing parameter estimate", sep='')) 
   ## Result list
-  res.list <- c(name.originalfit, call.originalfit, vector('list', length=p))
-  names(res.list) <- c('name.originalfit', 'call.originalfit', xnames[which.par])
-  ## If values are plotted a device is opened
+  res.list <- c(name.originalfit, call.originalfit,
+    vector('list', length=length(which.par)))
+  names(res.list) <- c('name.originalfit', 'call.originalfit',
+    xnames[which.par]) 
+  ## Open a device if fig = T
   if(fig) {
-    rows <- ceiling(sqrt(p))
-    op <- par(mfrow=c(rows, rows), oma = c(0,0,2,0))
+    nrows <- ceiling(sqrt(length(which.par[!par.null[which.par]])))
+    if(nrows > 3) {
+      op <- par(mfrow=c(3,3), mar = c(4.5,4,2,1), oma = c(1,1,5,1))
+      oask <- devAskNewPage()
+    }
+    else op <- par(mfrow=c(nrows, nrows), mar = c(4.5,4,2,1), oma = c(1,1,5,1))
     on.exit(par(op))
   } 
   ## For each wp calculate requisite values and plot them  
   for(wp in which.par) {
+    if(par.null[wp]) next
     par.wp <- x[[wp]]$par.values
     lik.wp <- x[[wp]]$lroot
-    spline.wp <- spline(par.wp, lik.wp) # husk tilføj n=n
+    spline.wp <- spline(par.wp, lik.wp, n = n)
     wp.name <- xnames[wp]
-    if(approx) approx.wp <- (par.wp - b0[wp.name])/std.err[wp.name]
-### byttes om på par.wp og b0?
+    if(approx) {
+      approx.wp <- (par.wp - b0[wp.name])/std.err[wp.name]
+      spline.approx.wp <- spline(par.wp, approx.wp, n = n)
+    }
     if(conf.int) cutoff.wp <- qnorm((1-level)/2, lower.tail = F) 
     if(statistic == 'lroot') {
       ylab <- "Profile likelihood root"
-      if(conf.int) cutoff.wp <- c(-1, 1) * cutoff.wp
+      if(conf.int) cutoff.wp <- c(-1, 1) %o% cutoff.wp
     } ## end 'if statistic is lroot'
-    else if(statistic == 'likelihood') {
-      ylab <- "Profile log-likelihood"
+    else { ## if statistic is likelihood
       spline.wp$y <- -spline.wp$y^2/2
-      if(approx) approx.wp <- -approx.wp^2/2
+      if(approx) spline.approx.wp$y <- -spline.approx.wp$y^2/2
       if(conf.int) cutoff.wp <- -cutoff.wp^2/2
-      if(!log)
-        ylab <- "Profile likelihood"
+      if(relative && log)
+        ylab <- "Rel. profile log-likelihood"
+      if(relative && !log) {
+        ylab <- "Rel. profile likelihood"
         spline.wp$y <- exp(spline.wp$y)
-        if(approx) approx.wp <- exp(approx.wp)
+        if(approx) spline.approx.wp$y <- exp(spline.approx.wp$y)
         if(conf.int) cutoff.wp <- exp(cutoff.wp)
+      }
+      if(!relative && log) {
+        ylab <- "Profile log-likelihood"
+        spline.wp$y <- spline.wp$y + lLik
+        if(approx) spline.approx.wp$y <- spline.approx.wp$y + lLik
+        if(conf.int) cutoff.wp <- cutoff.wp + lLik
+      }
+      if(!relative && !log) {
+        ylab <- "Profile likelihood"
+        spline.wp$y <- exp(spline.wp$y + lLik)
+        if(approx) spline.approx.wp$y <- exp(spline.approx.wp$y + lLik)
+        if(conf.int) cutoff.wp <- exp(cutoff.wp + lLik)
+      }
     } ## end 'if statistic is likelihood'
-    else
-      stop("Type of statistic not recognized. Supported arguments are 'likelihood' and 'lroot'")  
     res.list[[wp.name]]$spline.vals <- spline.wp
-    if(approx) res.list[[wp.name]]$approx.vals <- list(x = par.wp,
-                  y = approx.wp) 
-    if(conf.int) res.list[[wp.name]]$cutoff <- cutoff.wp
-
+    if(approx) res.list[[wp.name]]$spline.approx.vals <- spline.approx.wp 
+    if(conf.int) res.list[[wp.name]]$cutoff <- cutoff.wp 
     if(fig) {
-      plot(spline.wp$x, spline.wp$y, type='l', xlab=wp.name,
-      ylab=ylab, col='black')
+      plot(spline.wp$x, spline.wp$y, type='n', xlab=wp.name,
+      ylab=ylab, col='black', ylim = ylim)
       if(statistic=='lroot') points(b0[wp.name], 0, pch=18)
       if(conf.int) abline(h = cutoff.wp)
       if(approx) {
-        lines(par.wp, approx.wp, lty=2, col='steelblue')
-        legend('topleft', legend=c(statistic, 'approx'), lty = 1:2,
-        col=c('black','steelblue'), bty = 'n')
+        lines(spline.approx.wp$x, spline.approx.wp$y, lty=2,
+          col='steelblue')
       }
-      title(paste("\nProfile likelihood of parameter
-      estimates from object '", name.originalfit, "'", sep=""), outer=T)
+      lines(spline.wp$x, spline.wp$y)
+      title(paste("\nProfile likelihood of parameter estimates from object '",
+      name.originalfit, "'", sep=""), outer=T)
+      if(nrows > 3) {
+        devAskNewPage(TRUE)
+        on.exit(devAskNewPage(oask))
+      }
     } ## end 'if fig = T draw plot for each wp'
   } ## end 'for wp in which.par calculate values needed for plotting'
-#    attr(spline.list, '')
-    if(fig) invisible(res.list)
-    else return(res.list)
+  if(fig) invisible(res.list)
+  else return(res.list)
 }
-
-cc <- aa(pr1, statistic='lik', conf.int=T, approx=T)
-cc
-
 
 
 
